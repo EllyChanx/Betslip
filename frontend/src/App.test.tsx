a7d3f8a3cccfa06184c6b8f5f325bd5940965139
Elly Chan <echan@kiwipowered.com>
2021-07-28 11:57:17 +0100

    Add semantic ui lib

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import App from './App'
 import { mount } from 'enzyme';
 import './testSetup'
+import { Container } from 'semantic-ui-react';
 
 describe('<App>', () => {
 	it('', () => {
 		const wrapper = mount(<App />)
-		expect(wrapper.find('.App').length).toEqual(1)
+		expect(wrapper.find(Container).length).toEqual(1)
 	})
 })
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css'
+import { Container } from 'semantic-ui-react';
 
 function App() {
 	return (
-		<div className="App">
+		<Container>
 			Hello World!
-		</div>
+		</Container>
 	)
 }
 
